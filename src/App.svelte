af8d7c7c75a978041be68b54cd030c58f35dbf92
Priscilla (Sandra Laila) <a.g.marosi@bluewin.ch>
2025-03-18 13:16:43 +0100

    Implemented ToDo item. Adjusted VSCode local settings

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Item from "./lib/Item.svelte";
 </script>
 
 <main>
@@ -20,14 +21,18 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 
   <button>Button</button>
+
+  <Item />
 </main>
 
 <style>
---
+++ src/lib/Item.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  let done = $state(false);
+  let title = "Title of Task";
+  let desc =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
+</script>
+
+<div class="item {done ? 'done' : ''}">
+  <label class="label">
+    <input bind:checked={done} type="checkbox" />
+    <h1 class="title">{title}</h1>
+  </label>
+  <p class="desc">{desc}</p>
+</div>
+
+<style>
+  .item {
+    border: 1px solid #fff;
+    border-radius: 10px;
+    padding: 0.5rem 0.75rem;
+
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: stretch;
+    text-align: justify;
+
+    .label {
+      display: flex;
+      flex-direction: row;
+      justify-items: center;
+
+      margin-bottom: 0.75rem;
+
+      .title {
+        font-size: 2.25rem;
+        margin: 0;
+        padding-left: 0.25em;
+        border-radius: 0.25em;
+      }
+    }
+
+    .desc {
+      margin: 0;
+    }
+  }
+
+  .done {
+    border-color: #aaa;
+    background-color: #202020;
+
+    .title,
+    .desc {
+      color: #aaa;
+    }
+  }
+</style>
