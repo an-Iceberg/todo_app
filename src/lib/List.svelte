251667b3475e2110fdc7a92e555f634fa77027c2
an-Iceberg <a.g.marosi@bluewin.ch>
2025-03-19 22:14:06 +0100

    Added boilerplate for list

---
+++ src/lib/List.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Item from "./Item.svelte";
+
+  // Todo: create list type
+  // Todo: create task type with title, description, done
+</script>
+
+<Item />
+
+<style>
+</style>
